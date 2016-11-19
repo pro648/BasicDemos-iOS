54988917c7dce46628655fe224d5a5fb39d785c3
pro648 <20367411+pro648@users.noreply.github.com>
2016-11-19 15:45:57 +0800

    add persistence demo

---
+++ 数据持久化/Persistence/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Persistence
+//
+//  Created by ad on 19/11/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 数据持久化/Persistence/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Persistence
+//
+//  Created by ad on 19/11/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
